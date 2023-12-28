414142f6cf111b254e8fc6fc741b70764262cb57
Matheus Vieira <matvno@gmail.com>
2023-12-28 13:30:55 -0300

    Extract the ErrorMessage component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button, Callout, Text, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextField } from '@radix-ui/themes'
 import SimpleMDE from 'react-simplemde-editor'
 import 'easymde/dist/easymde.min.css'
 import { useForm, Controller } from 'react-hook-form'
@@ -11,6 +11,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
 import { createIssueSchema } from '@/app/validationSchemas'
+import ErrorMessage from '@/app/components/ErroMessage'
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
@@ -41,18 +42,16 @@ const NewIssuePage = () => {
             await axios.post('/api/issues', data)
             router.push('/issues')
           } catch (error) {
-            setError('An unexpected error occrurred.')
+            setError('An unexpected error occurred.')
           }
         })}
       >
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register('title')} />
         </TextField.Root>
-        {errors.title && (
-          <Text color="red" as="p">
-            {errors.title.message}
-          </Text>
-        )}
+
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+
         <Controller
           name="description"
           control={control}
@@ -61,11 +60,7 @@ const NewIssuePage = () => {
           )}
         />
 
-        {errors.description && (
-          <Text color="red" as="p">
-            {errors.description.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
         <Button>Submit New Issue</Button>
       </form>
