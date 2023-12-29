0067363437522f3451ad9ac7e34dd872b35abbea
Matheus Vieira <matvno@gmail.com>
2023-12-29 19:57:03 -0300

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -20,3 +20,13 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
 
   return NextResponse.json(updatedIssue, { status: 200 })
 }
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+  const issue = await prisma.issue.findUnique({ where: { id: Number(params.id) } })
+  if (!issue)
+    return NextResponse.json({ error: 'Issue not found' }, { status: 404 })
+
+  await prisma.issue.delete({ where: { id: issue.id } })
+
+  return NextResponse.json({ message: 'Issue deleted' }, { status: 200 })
+}
