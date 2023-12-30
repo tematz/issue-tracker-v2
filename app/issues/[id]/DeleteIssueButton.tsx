c411559e9502e8212331eebee9a6f3549ea2cdb2
Matheus Vieira <matvno@gmail.com>
2023-12-30 11:18:50 -0300

    Improve the user experience

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -5,16 +5,21 @@ import axios from 'axios'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
+import { Spinner } from '@/app/components'
+
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter()
   const [error, setError] = useState(false)
+  const [isDeleting, setDeleting] = useState(false)
 
   const deleteIssue = async () => {
     try {
+      setDeleting(true)
       await axios.delete(`/api/issues/${issueId}`)
       router.push('/issues')
       router.refresh()
     } catch (error) {
+      setDeleting(false)
       setError(true)
     }
   }
@@ -22,7 +27,9 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">Delete</Button>
+          <Button color="red" disabled={isDeleting}>
+            Delete {isDeleting && <Spinner />}
+          </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
           <AlertDialog.Title>Are you sure?</AlertDialog.Title>
