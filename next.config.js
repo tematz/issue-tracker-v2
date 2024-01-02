dccbab84b2ba1a080c20536df94386a4c7ca5a2e
Matheus Vieira <matvno@gmail.com>
2024-01-02 15:28:19 -0300

    Build the filter component

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/api/:path*',
+        source: '/:path*',
         headers: [
           { key: 'referrer-policy', value: 'no-referrer' },
         ]
