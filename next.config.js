8fe375ef5ccef18d2da7691bffac411d98fd4c7f
Matheus Vieira <matvno@gmail.com>
2024-01-02 12:45:10 -0300

    Fix avatar not loading

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'referrer-policy', value: 'no-referrer' },
+        ]
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
